59f94e40e691566f7431171290bc74c30d5c42b0
JuanBaezaB <51477488+JuanBaezaB@users.noreply.github.com>
2022-09-15 10:51:41 -0300

    Población inicial

--- n-reinas.py
+++ n-reinas.py
@@ -1,24 +1,48 @@
 import numpy as np
-import random
+import time
+import sys
+
+start = time.process_time()
+
+def pobl_inicial(tamaño_tabl, tamaño_pobl):
+    poblacion = np.zeros([tamaño_tabl, tamaño_pobl], dtype=int)
+    for k in range(tamaño_pobl):
+        poblacion[k] = np.arange(0,tamaño_tabl)
+        np.random.shuffle(poblacion[k])
+    return poblacion
+
+if len(sys.argv) == 7:
+    seed = int(sys.argv[1])
+    tamaño_tabl = int(sys.argv[2])
+    tamaño_pobl = int(sys.argv[3])
+    prob_cruza = float(sys.argv[4])
+    prob_mut = float(sys.argv[5])
+    num_ite = int(sys.argv[6])
+    print("seed: ", seed)
+    print("tamaño_tabl: ", tamaño_tabl)
+    print("tamaño_pobl : ", tamaño_pobl)
+    print("prob_cruza : ", prob_cruza)
+    print("prob_mut : ", prob_mut)
+    print("num_ite : ", num_ite)
+else:
+    print('Error en la entrada de los parametros')
+    print('Los paramentros a ingresar son: semilla TamañoTablero TamañoPoblación ProbabilidadCruza ProbabilidadMutación NumeroIteración')
+    sys.exit(0)
 
 
-seed, tamaño_tabl, tamaño_pobl, prob_cruza, prob_mut, num_ite  = map(int, input("Ingresa 6 valores: ").split())
+np.random.seed(seed)
 
-print("seed: ", seed)
-print("tamaño_tabl: ", tamaño_tabl)
-print("tamaño_pobl : ", tamaño_pobl)
-print("prob_cruza : ", prob_cruza)
-print("prob_mut : ", prob_mut)
-print("num_ite : ", num_ite)
 
-random.seed(seed)
+print(pobl_inicial(tamaño_tabl, tamaño_pobl))
 
-table = np.array([i for i in range(1, tamaño_tabl+1)])
-print(table)
 
-random.shuffle(table)
 
-print(table)
 
+
+
+
+# tiempo ejecución
+end = time.process_time()
+print('Tiempo de ejecución:', end - start,'segundos')
 
 
